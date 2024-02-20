e94a2601043118f3b18b5f91086d7b85934d4859
Adem Zitouni <adam.zitouni@esprit.tn>
2024-02-20 17:35:28 +0100

    interface supprimer

--- src/main/java/controllers/AfficherParking.java
+++ src/main/java/controllers/AfficherParking.java
@@ -4,6 +4,8 @@ import entities.Parking;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,19 +29,53 @@ public class AfficherParking {
     @FXML
     private TableColumn<Parking, Integer> colCapacite;
 
+    @FXML
+    private TableColumn<Parking, Void> colAction;
+
+    @FXML
+    private TableColumn<Parking, Void> colDelete;
+
     private final IService<Parking> serviceParking = new ServiceParking();
 
     @FXML
     void initialize() throws SQLException  {
 
-            Set<Parking> parkings = serviceParking.getAll();
-            ObservableList<Parking> parkingList = FXCollections.observableArrayList(parkings);
+        Set<Parking> parkings = serviceParking.getAll();
+        ObservableList<Parking> parkingList = FXCollections.observableArrayList(parkings);
 
-            colPlace.setCellValueFactory(new PropertyValueFactory<>("place"));
-            colNumPlace.setCellValueFactory(new PropertyValueFactory<>("numPlace"));
-            colCapacite.setCellValueFactory(new PropertyValueFactory<>("capacite"));
+        colPlace.setCellValueFactory(new PropertyValueFactory<>("place"));
+        colNumPlace.setCellValueFactory(new PropertyValueFactory<>("numPlace"));
+        colCapacite.setCellValueFactory(new PropertyValueFactory<>("capacite"));
 
-            tableParking.setItems(parkingList);
+        // Ajout de la colonne de suppression
+        colDelete.setCellFactory(param -> new TableCell<>() {
+            private final Button deleteButton = new Button("Supprimer");
 
+            @Override
+            protected void updateItem(Void item, boolean empty) {
+                super.updateItem(item, empty);
+
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    deleteButton.setOnAction(event -> {
+                        Parking parking = getTableView().getItems().get(getIndex());
+                        // Supprimer l'élément
+                        try {
+                            serviceParking.supprimer(parking.getIdParking());
+                        } catch (SQLException e) {
+                            throw new RuntimeException(e);
+                        }
+                        tableParking.getItems().remove(parking);
+                    });
+
+
+                    setGraphic(deleteButton);
+                }
+            }
+        });
+
+        tableParking.setItems(parkingList);
     }
+
 }
--- src/main/java/services/ServiceVoiture.java
+++ src/main/java/services/ServiceVoiture.java
@@ -138,4 +138,6 @@ public class ServiceVoiture implements IService<Voiture>{
         return voitures;
     }
 
+
+
 }
