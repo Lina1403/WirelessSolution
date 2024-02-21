f616a1fe787e5432b59a55ce0b5a73f99b29a92d
ChihebDabebi <dabebi.chiheb@esprit.tn>
2024-02-21 18:00:34 +0100

    suppression d'une discussion

--- src/main/java/controllers/MessageController.java
+++ src/main/java/controllers/MessageController.java
@@ -3,13 +3,16 @@ import entities.Message;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.*;
+import services.DiscussionService;
 import services.MessageService;
 import utils.MessageCell;
 
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class MessageController {
     @FXML
@@ -25,11 +28,41 @@ public class MessageController {
     private Button sendButton;
 
     public static int discuId ;
+    MessageService ms = new MessageService();
+    DiscussionService ds = new DiscussionService();
 
     public void initialize() throws SQLException {
-        MessageService ms = new MessageService();
         ObservableList<Message> messages = FXCollections.observableList(ms.afficherByDiscussionId(discuId));
         messageList.setItems(messages);
         messageList.setCellFactory(param -> new MessageCell());
     }
+    public void supprimerDiscussion()  {
+      try {
+          Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+          alert.setTitle("Confirmation ");
+          alert.setHeaderText("Look, a Confirmation Dialog");
+          alert.setContentText("Are you ok with deleting the discussion?");
+          Optional<ButtonType> result = alert.showAndWait();
+          if (result.get() == ButtonType.OK){
+              ds.supprimer(discuId);
+              Alert alert1 = new Alert(Alert.AlertType.INFORMATION);
+              alert1.setTitle("success ");
+              alert1.setContentText("discussion deleted successfully");
+              alert1.showAndWait();
+              changeScene();
+          }
+      }catch(SQLException e){
+          System.out.println(e.getMessage());
+      }
+    }
+    public void changeScene() {
+        try {
+            // Chargez le fichier FXML pour la nouvelle scène
+            Parent root = FXMLLoader.load(getClass().getResource("/ListeDiscussion.fxml"));
+            messageList.getScene().setRoot(root);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
